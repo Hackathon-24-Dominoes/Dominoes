c18775944473bb3e2866913c85a2d811109259b2
antunishdPursuit <dennysantunish@pursuit.org>
2024-12-10 15:18:55 -0500

    create modal component, merged modal with app

---
+++ src/Components/modal.jsx
@@ -0,0 +1,18 @@
+import '../App.css';
+
+function Modal({ isOpen, onClose, children }) {
+    if (!isOpen) return null;
+  
+    return (
+      <div className="modal-overlay">
+        <div className="modal">
+          {children}
+          <button className="close-button" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    );
+}
+
+export default Modal;
