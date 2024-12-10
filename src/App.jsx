96e1c5a7879f5f14a5428bf27ba7800c7b03cb4e
Kenny <ksecond@pursuit.org>
2024-12-10 17:23:14 -0500

    game logic, ken

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,54 @@ import LandingPage from "./pages/LandingPage";
 import StoryPage from "./pages/StoryPage";
 
 import "./App.css";
+<<<<<<< HEAD
+=======
+import Anthropic from "@anthropic-ai/sdk";
+import Modal from "./Components/modal";
+import React from 'react';
+import Game from "./Components/Game";
+
+const client = new Anthropic({
+  apiKey: import.meta.env.VITE_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+function choose(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateStoryPrompt() {
+  const themes = [
+    "Global Diplomacy Prevention",
+    "Quantum Probability Meditation",
+    "Squirrel Communication Training",
+    "Apocalypse Preparedness Drill",
+    "Reverse Psychology Therapy",
+    "Supernatural Summoning Technique",
+    "Interpretive Dance Choreography Inspiration",
+    "Computational Chaos Theory Experiment",
+    "Alien First Contact Protocol",
+    "Retirement Planning through Randomness",
+    "Quantum Entanglement Visualization",
+    "Your Dad thighs",
+    "Time Travel Rehearsal",
+    "Your Mom",
+    "Gangster rapper",
+    "Dog ate my homework",
+    "Raccoon overlords",
+  ];
+  const subjects = ["The world", "Your family", "Yourself"];
+  return `Ludicrous reason that I should play dominoes involving ${choose(
+    themes
+  )} with subject ${choose(
+    subjects
+  )}.3 sentences maximum and in an story format.`;
+}
+>>>>>>> 5d496bb (game logic, ken)
 
 function App() {
   return (
+<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
@@ -18,5 +63,14 @@ function App() {
     </Router>
   )
 };
+=======
+    <>
+    <Game></Game>
+      <Modal isOpen={isModalOpen} onClose={closeModal} msg={msg} getGreetingStory={getGreetingStory} >
+      </Modal>
+    </>
+  );
+}
+>>>>>>> 5d496bb (game logic, ken)
 
 export default App;
